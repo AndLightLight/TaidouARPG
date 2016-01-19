d4f1e6e34fd79aaf6a3998298d03d2b964ac7c96
HawardLocke <1534921818@qq.com>
2016-01-19 21:17:13 +0800

    some ui stuf

---
+++ Assets/GodScript/logic/ui/LoginScene.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+
+public class LoginScene : MonoBehaviour
+{
+    void Awake()
+    {
+        if (null != UIControl.Instance.UIRootObj)
+        {
+            GameObject.DestroyImmediate(UIControl.Instance.UIRootObj);
+        }
+        MainScene.bInit = false;
+    }
+
+    // Use this for initialization
+	void Start ()
+    {
+        SceneManager.Instance.mSceneType = GameDefines.SceneType.ST_LOGIN;
+
+        // UI Init
+		UIControl.Instance.Init(UIControl.UIType.Login);
+		
+		ReYun.Instance.Game_Event_CheckPoint(@"登录场景加载成功");
+	}
+}
