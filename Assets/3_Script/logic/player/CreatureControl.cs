1c97b1299999184144a03715e00f3451a418b883
HawardLocke <1534921818@qq.com>
2016-01-24 03:36:58 +0800

    late night

---
+++ Assets/3_Script/logic/player/CreatureControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatureControl : IObject
+{
+    public float speed;
+
+	protected override void SelfStart()
+	{
+		
+	}
+
+	protected override void SelfUpdate()
+	{
+
+	}
+
+	public bool HandleEvent(int eventValue)
+	{
+		return false;
+	}
+}
