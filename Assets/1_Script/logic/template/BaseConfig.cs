8bb174a10ba5df000a4c12ad6401b885c3a26fc6
HawardLocke <1534921818@qq.com>
2016-01-27 23:37:06 +0800

    template and json

---
+++ Assets/1_Script/logic/template/BaseConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+public class BaseConfig
+{
+	private int _id = 0;
+	public int id { get { return _id; } }
+
+	private string _strId;
+	public string strId { get { return _strId; } }
+
+	public BaseConfig()
+	{
+
+	}
+
+	public virtual void init(JObject json)
+	{
+		if (false == int.TryParse(json["id"].ToString(), out _id))
+			_strId = json["id"].ToString();
+	}
+
+}
